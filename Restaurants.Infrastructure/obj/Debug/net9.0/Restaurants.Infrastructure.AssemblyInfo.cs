b70715851253db136cd296c053e2fe9b9946cdaa
Maynor de Leon <maynoralex@gmail.com>
2025-03-16 23:10:36 +0100

    remove files not needed on git with gitignore

--- Restaurants.Infrastructure/obj/Debug/net9.0/Restaurants.Infrastructure.AssemblyInfo.cs
+++ Restaurants.Infrastructure/obj/Debug/net9.0/Restaurants.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99aa044336085d788b7243e7452bea6844c0a0c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

b6a42bdfef97e0d741aae14a2358608b76e30941
Maynor de Leon <maynoralex@gmail.com>
2025-03-18 18:27:13 +0100

    Implementing Error Handling Middleware

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,4 +1,5 @@
 using Restaurants.API.Controllers;
+using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
@@ -16,6 +17,7 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Host.UseSerilog((context, configuration) => {
     configuration.ReadFrom.Configuration(context.Configuration);
 });
@@ -25,6 +27,7 @@ var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
 await seeder.Seed();
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseSerilogRequestLogging();
 

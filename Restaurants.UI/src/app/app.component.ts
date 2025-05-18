c7b8c460cf0fc10b31df92f6bc3f6db23ca055e2
Maynor de Leon <maynoralex@gmail.com>
2025-05-18 22:54:40 +0200

    Adding Angular App to Use WebAPI

---
+++ Restaurants.UI/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    HttpClient
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Restaurants.UI';
+}
